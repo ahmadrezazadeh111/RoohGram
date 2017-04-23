09d5c41eba9dc5af1c2fa69d7f269a1aa2a2ed90
ahmadrezazadeh111 <ahmadrezazadeh111@gmail.com>
2017-04-22 22:49:57 -0700

    Update BuildVars.java

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -13,8 +13,8 @@ public class BuildVars {
     public static boolean DEBUG_PRIVATE_VERSION = false;
     public static int BUILD_VERSION = 957;
     public static String BUILD_VERSION_STRING = "3.18";
-    public static int APP_ID = 57877; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
-    public static String APP_HASH = "6ce70b1b7cfa6e3c239449b7b183eca5"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+    public static int APP_ID = ; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
+    public static String APP_HASH = ""; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
     public static String HOCKEY_APP_HASH = "your-hockeyapp-api-key-here";
     public static String HOCKEY_APP_HASH_DEBUG = "your-hockeyapp-api-key-here";
     public static String BING_SEARCH_KEY = ""; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
